2e273330db3d6191e4b25f4a9794ba44e01b0b3e
Mohit Gupta <mohit.gupta@arcsinfotech.com>
2024-05-01 16:05:07 +0530

    initial app setup

---
+++ components/ConfigureAmplify.tsx
@@ -0,0 +1,11 @@
+// components/ConfigureAmplify.tsx
+"use client";
+
+import config from "@/amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(config, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+  return null;
+}
