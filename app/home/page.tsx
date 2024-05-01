58f467e34fa2351a612a9893503c93a8a92480c2
Mohit Gupta <mohit.gupta@arcsinfotech.com>
2024-05-01 19:01:40 +0530

    authenticated user check

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+    <>
+      <h1>Hello World from Demo Next App using Amplify Gen 2 👋</h1>
+      Signout
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
 "use client";
 
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import { getCurrentUser } from "aws-amplify/auth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 function App() {
+  const router = useRouter();
+
+  useEffect(() => {
+    getAuthUser();
+  },  []);
+  async function getAuthUser() {
+    try {
+      const response = await getCurrentUser();
+      console.log(response);
+      router.replace("home")
+    } catch (error) {
+      console.log(error);
+      router.replace(
+        "https://test-okta-ryan.auth.ap-south-1.amazoncognito.com/login?client_id=5desfnc8kkb036i0lt7fpkl6gf&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fmain.dd0uraac8ucm7.amplifyapp.com%2F"
+      );
+    }
+  }
   return (
     <>
-      <h1>Hello World from Demo Next App using Amplify Gen 2 👋</h1>
+      {/* <Authenticator></Authenticator> */}
+      <button onClick={getAuthUser}>Click</button>
     </>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
